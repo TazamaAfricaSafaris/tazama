a0f6d47015d8a4288457da12189a08188a66ed74
stanmelchizedek <stanexseven@gmail.com>
2024-06-26 11:52:39 +0300

    fixed content section component's bugs

--- src/components/ContentSection.tsx
+++ src/components/ContentSection.tsx
@@ -1,6 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
-import { MotionValue, motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
 import Link from "next/link";
 
 export type contentSectionData = {
@@ -9,132 +8,43 @@ export type contentSectionData = {
   title: string;
   action?: string;
   reverse: boolean;
-  description: string;
+  description: string | string[];
   actionTitle?: string;
 };
 
-// Custom hook for creating a parallax effect, based on scroll position and a distance value
-function useParallax(value: MotionValue<number>, distance: number) {
-  return useTransform(value, [0, 1], [-distance, distance]);
-}
-
-// content section memorized function component
 const ContentSection = (props: contentSectionData) => {
-  // Using a ref to track the scrollable element
-  const ref = useRef(null);
-  // Accessing scroll progress for the element referred to by `ref`
-  const { scrollYProgress } = useScroll({ target: ref });
-  // Applying the parallax custom hook to get a motion value for the y-axis
-  const y = useParallax(scrollYProgress, 100);
-  // JSX returned by the ContentSection component
+  const renderDescription = (description: string | string[]) => {
+    if (typeof description === "string") {
+      return description.split('\n').map((line, index) => (
+        <p key={index} className="mb-4">{line}</p>
+      ));
+    } else if (Array.isArray(description)) {
+      return description.map((line, index) => (
+        <p key={index} className="mb-4">{line}</p>
+      ));
+    }
+  };
+
   return (
-    // Fragment to return multiple elements without adding extra nodes to the DOM
     <>
-      {/* Wrapper div for the content section with conditional class names based on props */}
-      <div
-        className={`
-        -mt-30
-        flex
-        min-h-screen
-        w-full
-        flex-col-reverse
-        items-center overflow-hidden
-        sm:mt-52
-        lg:mt-0
-        ${props.reverse ? "justify-between md:flex-row-reverse" : "md:flex-row"}
-      `}
-      >
-        {/* The motion.div contains the image and will move up/down as the user scrolls (parallax effect) */}
-        <motion.div style={{ y }} className={`h-[75vh] w-full md:max-w-[50%]`}>
-          {/* Image element with dynamic source and class names */}
-          <img
-            src={`/assets/images/gallery/${props.image}`}
-            alt={props.title}
-            className={`h-full w-full ${
-              props.reverse ? "ml-4 sm:ml-10" : "-ml-4 sm:-ml-10"
-            } object-cover`}
-          />
-        </motion.div>
-
-        {/* Container for the text content */}
-        <div
-          ref={ref}
-          className={` z-10 mt-10  w-full bg-[#f4f1ec] px-6 md:max-w-[55rem] md:px-10 lg:px-20 `}
-        >
-          {/* Heading with fade and slide-in animation when it comes into view */}
-          <motion.h3
-            whileInView={{
-              x: 0,
-              opacity: 1,
-              transition: {
-                duration: 0.5,
-              },
-            }}
-            initial={{ opacity: 0, x: -10 }}
-            className={`mb-4 pt-10 text-3xl text-black lg:text-5xl`}
-          >
-            {props.title}
-          </motion.h3>
-
-          {/* Paragraph with delayed fade and slide-in animation */}
-          <motion.p
-            initial={{ opacity: 0, x: -10 }}
-            whileInView={{
-              opacity: 1,
-              x: 0,
-              transition: {
-                duration: 0.5,
-                delay: 0.2,
-              },
-            }}
-            className={`
-              font-raleway
-              mb-8
-              text-lg
-              text-[#757371]
-             
-            `}
-          >
-            {props.description}
-          </motion.p>
-
-          {/* Conditional rendering for the call-to-action button with animation */}
-
-          {props.action ? (
-            <motion.span
-              initial={{ opacity: 0, x: -10 }}
-              whileInView={{
-                opacity: 1,
-                x: 0,
-                transition: {
-                  duration: 0.5,
-                  delay: 0.4,
-                },
-              }}
-            >
-              <Link
-                href={props.action}
-                className={`
-                    font-raleway
-                    rounded-md
-                    border
-                    border-black
-                    px-8
-                    py-3
-                    transition
-                    duration-150
-                    ease-in-out
-                    hover:border-[#A87133]
-                    hover:bg-[#A87133]
-                    hover:text-white
-                  `}
-              >
-                {props.actionTitle}
-              </Link>
-            </motion.span>
-          ) : null}
-          <br />
-          <br />
+      <div className={`flex flex-col ${props.reverse ? "lg:flex-row" : "lg:flex-row-reverse"} gap-6 lg:items-start lg:mt-0 mt-10`}>
+        <div className="w-full px-10 md:px-20 lg:py-12 lg:px-32 sticky lg:top-1">
+          <h3 className="text-5xl mb-4 font-light font-amiora capitalize">{props.title}</h3>
+          {renderDescription(props.description)}
+          {
+            props.action && (
+              <>
+                <Link href={props.action} className="border border-primary rounded-md px-6 py-2 mt-4 text-xl font-raleway text-primary hover:bg-primary hover:text-white transition-colors duration-200">
+                  {props.actionTitle}
+                </Link>
+                <br />
+                <br />
+              </>
+            )
+          }
+        </div>
+        <div className={`w-full scroll-up h-72 md:h-96 lg:h-[95vh] xl:h-[85vh] overflow-hidden`}>
+          <img src={`/assets/images/gallery/${props.image}`} alt="" className={`w-full h-full object-cover ${props.reverse ? "ml-10 md:ml-20 lg:ml-0" : "-ml-10 md:-ml-20 lg:ml-0"}`} />
         </div>
       </div>
     </>
