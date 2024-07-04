156377095d8141b42fcece2307a1f9e0c9f51885
stanmelchizedek <stanexseven@gmail.com>
2024-07-04 16:50:34 +0300

    edited the blogs page as well as the data

--- src/blogs/all-blogs.ts
+++ src/blogs/all-blogs.ts
@@ -1,70 +1,90 @@
 export const allBlogs = [
   {
     id: 1,
+    category: "Trekking",
     name: "The Essential Guide to The best Sunglasses for Climbing Mount Kilimanjaro",
+    shortDescription: "As you set your sights on conquering Mount Kilimanjaro, the tallest peak in Africa, it's not just physical fitness and mental fortitude that will carry you to the summit.",
     url: "/safaris/kilimanjaro/blogs/best-sunglasses-for-kilimanjaro",
     imgUrl:
       "https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c3VuZ2xhc3Nlc3xlbnwwfHwwfHx8MA%3D%3D",
   },
   {
     id: 2,
-    name: "How Climbing Mount Kilimanjaro Reshapes Your View of Life",
+    category: "Trekking",
+    name: "How to Climbing Mount Kilimanjaro Reshapes Your View of Life",
+    shortDescription: "The path to Kilimanjaro's summit is a great leveler. It strips away all pretenses, revealing the core of human endurance and spirit. Here, on these ancient trails, you learn a humbling truth: we are all equal in the face of nature's majesty.",
     url: "/safaris/kilimanjaro/blogs/climbing-kili-shapes-your-life",
     imgUrl:
       "https://images.unsplash.com/photo-1631646109206-4b5616964f84?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a2lsaW1hbmphcm98ZW58MHx8MHx8fDA%3D",
   },
   {
     id: 3,
+    category: "Trekking",
     name: "Tazama's Diverse Kilimanjaro Climbers",
+    shortDescription: "Tazama has been at the forefront of turning these dreams into reality, offering unparalleled experiences to climbers from diverse nations including Australia, Canada, Germany, the USA, China, Switzerland, the UK, and beyond.",
     url: "/safaris/kilimanjaro/blogs/diverse-kilimanjaro-climbers",
     imgUrl:
       "https://images.unsplash.com/photo-1621414050946-1b936a78491f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8a2lsaW1hbmphcm98ZW58MHx8MHx8fDA%3D",
   },
   {
     id: 4,
+    category: "Trekking",
     name: "Your Mount Kilimanjaro Family",
+    shortDescription: "The adventure of Climbing Mount Kilimanjaro isn't just about reaching the peak; it's about the joyous journey you undertake with your new mountain family.",
     url: "/safaris/kilimanjaro/blogs/kilimanjaro-family",
     imgUrl:
       "https://images.unsplash.com/photo-1635865897833-38bc0f8aee44?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGtpbGltYW5qYXJvfGVufDB8fDB8fHww",
   },
   {
     id: 5,
+    category: "Trekking",
     name: "Kilimanjaro's Tipping Ceremony",
+    shortDescription: "Tazama, with its commitment to excellence and genuine connections, cherishes the tradition of the tipping ceremony—a moment that transcends mere transaction and becomes a heartfelt expression of gratitude towards those who have become more than guides, porters, and cooks; they've become part of your journey's story.",
     url: "/safaris/kilimanjaro/blogs/kilimanjaro-tipping-ceremony",
     imgUrl:
       "https://images.unsplash.com/photo-1572540698311-a51eac1cf4d4?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGtpbGltYW5qYXJvfGVufDB8fDB8fHww",
   },
   {
     id: 6,
+    category: "Trekking",
     name: "Preventing Injuries on Mount Kilimanjaro with Tazama",
+    shortDescription: "At Tazama, we understand that the journey to Kilimanjaro's summit is not just about reaching the peak but doing so safely, with memories of awe-inspiring landscapes, not injuries.",
     url: "/safaris/kilimanjaro/blogs/preventing-injuries",
     imgUrl:
       "https://images.unsplash.com/photo-1589553416260-f586c8f1514f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGtpbGltYW5qYXJvfGVufDB8fDB8fHww",
   },
   {
     id: 7,
-    name: "WHAT IS RIGHT FOR ME - PRIVATE VS GROUP TREK",
+    category: "Trekking",
+    name: "What is right for me, private or group trek?",
+    shortDescription: "Mount Kilimanjaro, standing as a beacon of challenge and transformation, calls to adventurers seeking to push their limits and experience the profound beauty of the world's tallest free-standing mountain.",
     url: "/safaris/kilimanjaro/blogs/private-vs-group-trek",
     imgUrl:
       "https://images.unsplash.com/photo-1489392191049-fc10c97e64b6?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8a2lsaW1hbmphcm98ZW58MHx8MHx8fDA%3D",
   },
   {
     id: 8,
+    category: "Trekking",
     name: "Recovery Journey After climbing Kilimanjaro: How many days it takes to recover from climbing kilimanjaro",
+    shortDescription: "Let's explore how you can shield yourself from common injuries and ensure your Kilimanjaro climb is a testament to triumph and resilience.",
     url: "/safaris/kilimanjaro/blogs/recovery-journey",
     imgUrl:
       "https://images.unsplash.com/photo-1601461265802-1a7b89edf876?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fGtpbGltYW5qYXJvfGVufDB8fDB8fHww",
   },
   {
     id: 9,
+    category: "Trekking",
     name: "Your Summit Night on Kilimanjaro",
+    shortDescription: "As you prepare to conquer Mount Kilimanjaro, the pinnacle of your adventure awaits during the enigmatic summit night. This is the moment that tests your endurance, spirit, and determination under the veil of darkness.",
     url: "/safaris/kilimanjaro/blogs/summit-night",
     imgUrl:
       "https://images.unsplash.com/photo-1519299390871-0028583860dd?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGtpbGltYW5qYXJvfGVufDB8fDB8fHww",
   },
   {
     id: 10,
+    category: "Trekking",
     name: "Why Climb Kilimanjaro with Tazama?",
+    shortDescription: "At Tazama, we've witnessed countless adventurers transform through this journey, each with their unique motivation, but all united in the quest for that exhilarating moment of triumph atop Africa's highest peak. Let's delve into why Kilimanjaro beckons and how it has become a pivotal chapter in the lives of many.",
     url: "/safaris/kilimanjaro/blogs/why-climb-kilimanjaro",
     imgUrl:
       "https://images.unsplash.com/photo-1621414050345-53db43f7e7ab?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8a2lsaW1hbmphcm98ZW58MHx8MHx8fDA%3D",
@@ -72,70 +92,90 @@ export const allBlogs = [
 
   {
     id: 11,
-    name: "KILIMANJARO ALTITUDE SICKNESS",
+    category: "Trekking",
+    name: "Kilimanjaro Altitude Sickness",
+    shortDescription: "At Tazama, we've witnessed countless adventurers transform through this journey, each with their unique motivation, but all united in the quest for that exhilarating moment of triumph atop Africa's highest peak. Let's delve into why Kilimanjaro beckons and how it has become a pivotal chapter in the lives of many.",
     url: "/safaris/kilimanjaro/guides/altitude-sickness",
     imgUrl:
       "https://images.unsplash.com/photo-1609198093484-b4ab6db40ace?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGtpbGltYW5qYXJvfGVufDB8fDB8fHww",
   },
   {
     id: 12,
-    name: "ELECTRONIC GADGETS ON KILIMANJARO",
+    category: "Trekking",
+    name: "Electronic gadgets",
+    shortDescription: "In the majestic realm of Mount Kilimanjaro, where every step brings you closer to the roof of Africa, ensuring you're well-equipped is paramount. Tazama understands the essence of being prepared, especially when it comes to electronic gadgets that can transform your climb into a more comfortable and memor",
     url: "/safaris/kilimanjaro/guides/electronic-gadgets",
     imgUrl:
       "https://images.unsplash.com/photo-1521150932951-303a95503ed3?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzN8fGtpbGltYW5qYXJvfGVufDB8fDB8fHww",
   },
   {
     id: 13,
+    category: "Trekking",
     name: "Is Climbing Kilimanjaro Safe? Absolutely!",
+    shortDescription: "In the majestic realm of Mount Kilimanjaro, where every step brings you closer to the roof of Africa, ensuring you're well-equipped is paramount. Tazama understands the essence of being prepared, especially when it comes to electronic gadgets that can tran",
     url: "/safaris/kilimanjaro/guides/is-kilimanjaro-safe",
     imgUrl:
       "https://images.unsplash.com/photo-1554091669-6d00516d9cb3?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fGtpbGltYW5qYXJvfGVufDB8fDB8fHww",
   },
   {
     id: 14,
+    category: "Trekking",
     name: "Mount Kilimanjaro Facts: Unveiling Africa's Majestic Summit",
+    shortDescription: "Mount Kilimanjaro, a marvel of nature, stands as a beacon of adventure for climbers worldwide. Located in Tanzania, this giant isn't just any mountain; it's Africa's highest peak and the world's tallest free-standing mountain, offering unique challenges and breathtaking beauty.",
     url: "/safaris/kilimanjaro/guides/kilimanjaro-facts",
     imgUrl:
       "https://images.unsplash.com/photo-1618595713808-d6447ab3d9b4?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDh8fGtpbGltYW5qYXJvfGVufDB8fDB8fHww",
   },
   {
     id: 15,
+    category: "Trekking",
     name: "Kilimanjaro Food",
+    shortDescription: "Mount Kilimanjaro, a marvel of nature, stands as a beacon of adventure for climbers worldwide. Located in Tanzania, this giant isn't just any mountain; it's Africa's highest peak and the world's tallest free-standing mountain, offering uniqu",
     url: "/safaris/kilimanjaro/guides/kilimanjaro-food",
     imgUrl:
       "https://images.unsplash.com/photo-1585540083814-ea6ee8af9e4f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YWZyaWNhbiUyMGZvb2R8ZW58MHx8MHx8fDA%3D",
   },
   {
     id: 16,
-    name: "SOLO KILIMANJARO CLIMB",
+    category: "Trekking",
+    name: "Solo Kilimanjaro Climb",
+    shortDescription: "Embarking on a solo journey to conquer Mount Kilimanjaro, Africa's highest peak, is an aspiration many adventurers hold dear. Tazama is here to turn this dream into a reality, offering a tailored, luxurious experience for solo climbers seeking to tackle this magnificent mountain.",
     url: "/safaris/kilimanjaro/guides/kilimanjaro-solo-climb",
     imgUrl:
       "https://images.unsplash.com/photo-1609198092458-38a293c7ac4b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fGtpbGltYW5qYXJvfGVufDB8fDB8fHww",
   },
   {
     id: 17,
-    name: "VACCINATIONS FOR KILIMANJARO",
+    category: "Trekking",
+    name: "Vaccinations for Kililmanjaro",
+    shortDescription: "Conquering Mount Kilimanjaro, the tallest freestanding mountain in the world, is an exhilarating experience that demands meticulous preparation, especially when it comes to health and safety. Tazama is here to ensure that your adventure is not only memorable but also safeguarded against any health risks that ",
     url: "/safaris/kilimanjaro/guides/kilimanjaro-vaccinations",
     imgUrl:
       "https://images.unsplash.com/photo-1584515979956-d9f6e5d09982?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8dmFjY2luYXRpb25zfGVufDB8fDB8fHww",
   },
   {
     id: 18,
-    name: "DO YOU NEED A VISA TO CLIMB KILIMANJARO?",
+    category: "Trekking",
+    name: "Do You Need a VISA to Climb Kilimanjaro",
+    shortDescription: "Embarking on a journey to Mount Kilimanjaro, the crown jewel of Tanzania and Africa's highest peak, is a dream for many adventurers and trekkers worldwide. Before setting foot on this majestic mountain, there's a crucial step every traveler",
     url: "/safaris/kilimanjaro/guides/kilimanjaro-visa",
     imgUrl:
       "https://images.unsplash.com/photo-1581656702382-9ae90e68e7b7?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmlzYXxlbnwwfHwwfHx8MA%3D%3D",
   },
   {
     id: 19,
-    name: "PRIVATE TOILET TENTS ON KILIMANJARO",
+    category: "Trekking",
+    name: "Private Toilet Tents in Kilimanjaro",
+    shortDescription: "Tazama prides itself on enhancing the Kilimanjaro climbing experience with the provision of Private Toilet Tents, a service that guarantees privacy and convenience for all climbers. Recognizing",
     url: "/safaris/kilimanjaro/guides/private-toilets",
     imgUrl:
       "https://images.unsplash.com/photo-1619912813846-9b6c0dd846b0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHRvaWxldHN8ZW58MHx8MHx8fDA%3D",
   },
   {
     id: 20,
+    category: "Trekking",
     name: "Training for Kilimanjaro: Your Ultimate Preparation Guide",
+    shortDescription: "Start your Kilimanjaro training journey well in advance, several weeks or ideally several months before to ensure you're well-prepared for the summit of Africa's highest peak with our comprehensive guide. Climbing Mount Kilimanjaro is an adventure of a lifetime. As the r",
     url: "/safaris/kilimanjaro/guides/training-for-kilimanjaro",
     imgUrl:
       "https://images.unsplash.com/photo-1476480862126-209bfaa8edc8?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYWluaW5nfGVufDB8fDB8fHww",
