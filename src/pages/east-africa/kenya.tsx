349e262528d58d647b75094bb68dd92bca6a1308
stanmelchizedek <stanexseven@gmail.com>
2024-08-22 10:33:14 +0300

    added some content on kenya page

--- src/pages/east-africa/kenya.tsx
+++ src/pages/east-africa/kenya.tsx
@@ -4,7 +4,24 @@ import PrimaryHeader from '~/components/PrimaryHeader'
 const kenya = () => {
   return (
     <>
-        <PrimaryHeader title='Kenya' image=''/>
+      <PrimaryHeader title='Kenya' image='' />
+      <div className='max-w-5xl mx-auto my-20'>
+        <p className='text-left leading-relaxed'>
+          Kenya safaris offer a magical mix of wildlife adventures and scenic beauty, drawing visitors from all corners of the globe. Renowned for its incredible wildlife and world-famous safari destinations, Kenya offers a true African safari experience that is hard to match. Imagine seeing the Big Five (lion, leopard, buffalo, elephant, and rhinoceros) in iconic national parks like the Maasai Mara, Amboseli, and Tsavo, or relaxing on the pristine coastline near Mombasa.
+          <br /><br />
+          From spotting vast herds of animals during the Great Migration to exploring Kenya’s coastal treasures, this country offers something for every kind of traveler. With a rich diversity of landscapes and a warm, welcoming culture, Kenya safaris deliver unforgettable experiences.
+
+        </p>
+
+        <section className='my-16'>
+          <h3 className='text-5xl mb-2 text-primary'>Why Visit Kenya</h3>
+          <p className='leading-relaxed'>
+            Kenya’s landscapes are breathtaking, featuring a dramatic mix of calderas, mountain ranges, and the Great Rift Valley, which divides the country. Adventure seekers will love hiking Mount Kenya’s snow-capped peaks, while those seeking relaxation can enjoy the hot springs and natural geysers of Hell's Gate National Park. A Kenya safari combines all of these wonders with world-class wildlife viewing, making it a must-visit destination for nature lovers and adventurers alike.
+          </p>
+        </section>
+
+        
+      </div>
     </>
   )
 }
