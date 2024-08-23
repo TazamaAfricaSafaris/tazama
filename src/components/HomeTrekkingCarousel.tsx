eb69b2987df6d96f60c710c5ad5769c59cd03d56
stanmelchizedek <stanexseven@gmail.com>
2024-08-23 16:51:46 +0300

    added trekking content in homepage

--- src/components/HomeTrekkingCarousel.tsx
+++ src/components/HomeTrekkingCarousel.tsx
@@ -40,7 +40,7 @@ const HomeTrekkingCarousel = () => {
   }, [])
 
   return (
-    <div className='overflow-hidden'>
+    <div className='overflow-hidden my-16 lg:my-20'>
       <div ref={triggerRef}>
 
         <div ref={sectionRef} className='flex flex-row w-[200vw] relative h-screen'>
@@ -49,7 +49,7 @@ const HomeTrekkingCarousel = () => {
             <div className='z-20 absolute top-1/2 p-4 max-w-4xl left-[5%] -translate-y-1/2'>
               <h3 className='text-white text-4xl md:text-6xl'>Mount Kilimanjaro</h3>
               <p className='text-[#f5f1eb] text-sm md:text-base'>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum rem accusantium rerum minima sequi totam quisquam, nam quibusdam nobis laborum necessitatibus corrupti voluptas distinctio beatae. Architecto nisi quaerat optio. Nostrum?
+                Conquer the iconic Mount Kilimanjaro, the highest peak in Africa with Tazama Africa Safaris. Whether you're an experienced climber or a first-time trekker, this journey promises unforgettable memories and unparalleled views. Ready to take on the challenge? Explore our Kilimanjaro expeditions and start planning your ascent today!
               </p>
               <br />
               <Link href='safaris/kilimanjaro' className="border border-white rounded-md px-4 lg:px-6 py-2 mt-4 md:text-lg text-xl font-raleway text-white hover:bg-white/90 hover:text-primary transition-colors duration-200">
@@ -68,7 +68,7 @@ const HomeTrekkingCarousel = () => {
             <div className='z-20 absolute top-1/2 p-4 max-w-4xl left-[5%] -translate-y-1/2'>
               <h3 className='text-white text-4xl md:text-6xl'>Mount Meru</h3>
               <p className='text-[#f5f1eb] text-sm md:text-base'>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum rem accusantium rerum minima sequi totam quisquam, nam quibusdam nobis laborum necessitatibus corrupti voluptas distinctio beatae. Architecto nisi quaerat optio. Nostrum?
+                Discover the hidden gem of Tanzania with a trek to Mount Meru, the perfect introduction to the region's majestic peaks.  This climb is ideal for those seeking a less-traveled path with equally rewarding scenery. Ready to explore the beauty of Mount Meru? Learn more about our guided treks and begin your adventure now!
               </p>
               <br />
               <Link href='safaris/mount-meru' className="border border-white rounded-md px-4 lg:px-6 py-2 mt-4 md:text-lg text-xl font-raleway text-white hover:bg-white/90 hover:text-primary transition-colors duration-200">
