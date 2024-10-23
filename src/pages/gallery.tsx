ab2b8b7d8a22c787a5d5c15cf6024dd269bb9774
stanmelchizedek <stanexseven@gmail.com>
2024-10-23 10:49:20 +0300

    added masonry layout to gallery page and other gallery images

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,55 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
 import PrimaryHeader from "~/components/PrimaryHeader";
 import Image from "next/legacy/image";
+import { type ImageProps } from "~/lib/generateBlurPlaceHolder";
 import HeadSEO from "~/components/ui/Head";
 import { base_keywords } from "~/lib/constants";
-const honeyMooners = [
-  {
-    id: 1,
-    src: "/assets/images/gallery/family-trip.webp",
-  },
-  {
-    id: 2,
-    src: "/assets/images/gallery/maasai.webp",
-  },
-  {
-    id: 3,
-    src: "/assets/images/gallery/blog.webp",
-  },
-  {
-    id: 4,
-    src: "/assets/images/gallery/offer.webp",
-  },
-  {
-    id: 5,
-    src: "/assets/images/gallery/classic-safaris.webp",
-  },
-  {
-    id: 6,
-    src: "/assets/images/gallery/discovery.webp",
-  },
-];
+import useFetchImages from "~/hooks/useFetchImages";
+import { Masonry } from "react-plock"
 
-const MemoryImage = ({ src }: { src: string }) => {
-  return (
-    <div className="relative h-[200px] w-[80%] md:h-[300px] lg:w-[400px]">
-      <Image src={src} layout="fill" className="rounded-sm object-cover" />
-    </div>
-  );
-};
-
-const GalleryPage = () => {
+const GalleryPage = ({ images }: { images: ImageProps[] }) => {
   return (
     <>
       <HeadSEO title="Gallery" keywords={base_keywords} />
       <PrimaryHeader image="gallery.webp" title="Glimpse of our Memories" />
 
       <section className="flex flex-col items-center justify-center max-w-5xl mx-auto mt-16">
-        <div className="mb-4 py-8">
-          <h3 className="text-center text-4xl text-[#A87133]">
+        <div className="mb-4 py-8 px-6 lg:px-0">
+          <h3 className="text-center text-4xl md:text-5xl text-[#A87133]">
             Our Personal Tailored experiences
           </h3>
-          <p className="mt-5 text-[#757371]">
+          <p className="mt-5">
             Welcome to our diverse collection of meticulously crafted
             itineraries, each designed to offer you a unique and unforgettable
             travel experience. Our itineraries are thoughtfully curated to
@@ -58,13 +30,8 @@ const GalleryPage = () => {
           </p>
         </div>
         <div>
-          <div className="mt-[4px] flex flex-col items-center justify-center gap-4 md:gap-0 md:grid md:grid-cols-2 lg:grid-cols-3">
-            {honeyMooners.map((item) => (
-              <MemoryImage src={item.src} key={item.id} />
-            ))}
-            {honeyMooners.map((item) => (
-              <MemoryImage src={item.src} key={item.id} />
-            ))}
+          <div className=" mb-20 px-6 md:px-0">
+            <MasonryGallery images={images} />
           </div>
         </div>
       </section>
@@ -73,3 +40,43 @@ const GalleryPage = () => {
 };
 
 export default GalleryPage;
+
+function MasonryGallery({ images }: { images: ImageProps[] }) {
+  return (
+    <Masonry
+      items={images}
+      config={{
+        columns: [2, 2, 3],
+        gap: [6, 9, 12],
+        media: [640, 768, 1024],
+      }}
+      render={(image) => (
+        <div className="rounded-xl overflow-hidden">
+          <Image
+            alt="tazama gallery photos"
+            className="object-cover w-full h-full rounded-xl"
+            style={{
+              transform: "translate3d(0, 0, 0)",
+              imageRendering: "crisp-edges",
+            }}
+            placeholder="blur"
+            blurDataURL={image.blurDataUrl}
+            src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${image.public_id}.${image.format}`}
+            width={Number(image.width)}
+            height={Number(image.height)}
+          // layout="responsive"
+          />
+        </div>
+      )}
+    />
+  );
+}
+
+export async function getStaticProps() {
+  const images = await useFetchImages({ folderName: "gallery" });
+  return {
+    props: {
+      images,
+    },
+  };
+}
