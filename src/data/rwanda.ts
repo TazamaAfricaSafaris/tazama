2481eddc0988d0b2ff84ed4e4197b4c29d8085ba
stanmelchizedek <stanexseven@gmail.com>
2024-11-03 15:04:50 +0300

    added 4 day gorilla trekking page and some other new changes

---
+++ src/data/rwanda.ts
@@ -0,0 +1,31 @@
+import { type accordion } from "~/components/Accordion";
+
+export const fourDayGorillaTrekking: accordion[] = [
+    {
+      question: "Day 1: Arrival in Kigali – Transfer to Volcanoes National Park",
+      answer: "Upon arrival at Kigali International Airport, enjoy a warm welcome and a scenic 3-hour drive to Volcanoes National Park in Musanze. Check in at Singita Kwitonda Lodge, relax, and enjoy a delicious dinner prepared by the lodge's renowned chefs.",
+      listHeader: "Accommodation",
+      list: ["Singita Kwitonda Lodge – Suite (Laundry services included)"]
+    },
+    {
+      question: "Day 2: Gorilla Trekking and Conservation Insights",
+      answer: "After breakfast at the lodge, experience an unforgettable mountain gorilla trekking adventure in Volcanoes National Park. After lunch, visit the Dian Fossey Gorilla Fund's Ellen DeGeneres Campus to learn about important conservation efforts. End the day with dinner at the lodge.",
+      listHeader: "Accommodation",
+      list: ["Singita Kwitonda Lodge – Suite (Laundry services included)"]
+    },
+    {
+      question: "Day 3: Golden Monkey Trekking and Cultural Expedition",
+      answer: "Start with breakfast before embarking on golden monkey trekking in Volcanoes National Park. After lunch, visit the Gorilla Guardians Village for a cultural experience, including traditional music, dance, and crafts. Return to the lodge for dinner.",
+      listHeader: "Accommodation",
+      list: ["Singita Kwitonda Lodge – Suite (Laundry services included)"]
+    },
+    {
+      question: "Day 4: Unforgettable Farewell Day in Rwanda",
+      answer: "After breakfast, enjoy a scenic drive back to Kigali for a city tour, visiting the Kigali Genocide Memorial, art galleries, and craft markets. Have a gourmet lunch at Fusion Restaurant at the Retreat by Heaven before transferring to Kigali International Airport for departure.",
+      listHeader: "Activity Highlights",
+      list: ["Kigali Genocide Memorial", "Art Galleries", "Craft Markets"],
+      listHeader2: "Meal Plan",
+      list2: ["Breakfast and Lunch included"]
+    }
+  ];
+  
