3ff5d42ca43ad06c9a499d5d029a55ebec8fd6a6
stanmelchizedek <stanexseven@gmail.com>
2024-11-03 13:20:17 +0300

    added uganda gorilla safari fly in and updated accordion component

---
+++ src/data/uganda.ts
@@ -0,0 +1,39 @@
+import { type accordion } from "~/components/Accordion";
+
+export const ugandaGorillaSafariFlyIn: accordion[] = [
+  {
+    question: "Day 1: Arrival in Entebbe and Flight to Bwindi",
+    answer: "Arrive in Entebbe, meet with representative, and take a domestic flight to Bwindi. After landing, transfer to your lodge. Enjoy lunch and relax. Dinner included.",
+    listHeader: "Overnight Accommodations",
+    list: [
+      "Standard: Bweza Gorilla Lodge",
+      "Midrange: Ichumbi Gorilla Lodge",
+      "Luxury: Nkuringo Bwindi Gorilla Lodge"
+    ],
+    listHeader2: "Meal Plan",
+    list2: [
+      "Full Board"
+    ],
+    note: "Optional Activity ($30 per person): Batwa Cultural Trail – A 3-hour experience showcasing the traditional lifestyle of the Batwa people."
+  },
+  {
+    question: "Day 2: Gorilla Tracking Day",
+    answer: "After breakfast, attend a briefing at the park office, then track mountain gorillas. Spend 1-2 hours observing and photographing them. Return to lodge afterward and enjoy a relaxed afternoon. Dinner included.",
+    listHeader: "Overnight Accommodations",
+    list: [
+      "Standard: Bweza Gorilla Lodge",
+      "Midrange: Ichumbi Gorilla Lodge",
+      "Luxury: Nkuringo Bwindi Gorilla Lodge"
+    ],
+    listHeader2: "Meal Plan",
+    list2: [
+      "Full Board"
+    ],
+  },
+  {
+    question: "Day 3: Transfer to Airstrip and Flight Back to Entebbe",
+    answer: "After breakfast, transfer to the airstrip for your return flight to Entebbe. Optional dayroom with lunch or a Shoebill tour at Mabamba Swamp can be arranged.",
+    listHeader: "Meal Plan",
+    list: ["Breakfast Only"]
+  },
+];
