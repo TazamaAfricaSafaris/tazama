c6db34d97db07971a0b4a4265c57d7ddd07317f8
stanmelchizedek <stanexseven@gmail.com>
2024-11-04 12:53:35 +0300

    removed references to accomodations

--- src/data/uganda.ts
+++ src/data/uganda.ts
@@ -4,14 +4,8 @@ export const ugandaGorillaSafariFlyIn: accordion[] = [
   {
     question: "Day 1: Arrival in Entebbe and Flight to Bwindi",
     answer: "Arrive in Entebbe, meet with representative, and take a domestic flight to Bwindi. After landing, transfer to your lodge. Enjoy lunch and relax. Dinner included.",
-    listHeader: "Overnight Accommodations",
+    listHeader: "Meal Plan",
     list: [
-      "Standard: Bweza Gorilla Lodge",
-      "Midrange: Ichumbi Gorilla Lodge",
-      "Luxury: Nkuringo Bwindi Gorilla Lodge"
-    ],
-    listHeader2: "Meal Plan",
-    list2: [
       "Full Board"
     ],
     note: "Optional Activity ($30 per person): Batwa Cultural Trail – A 3-hour experience showcasing the traditional lifestyle of the Batwa people."
@@ -19,14 +13,8 @@ export const ugandaGorillaSafariFlyIn: accordion[] = [
   {
     question: "Day 2: Gorilla Tracking Day",
     answer: "After breakfast, attend a briefing at the park office, then track mountain gorillas. Spend 1-2 hours observing and photographing them. Return to lodge afterward and enjoy a relaxed afternoon. Dinner included.",
-    listHeader: "Overnight Accommodations",
+    listHeader: "Meal Plan",
     list: [
-      "Standard: Bweza Gorilla Lodge",
-      "Midrange: Ichumbi Gorilla Lodge",
-      "Luxury: Nkuringo Bwindi Gorilla Lodge"
-    ],
-    listHeader2: "Meal Plan",
-    list2: [
       "Full Board"
     ],
   },
@@ -42,68 +30,37 @@ export const ugandaChimpsWildlifeFishingSafari: accordion[] = [
   {
     question: "Day 1: Welcome to Uganda!",
     answer: "Arrive at Entebbe International Airport and meet your safari guide. Transfer to Kampala, check into your hotel, and enjoy some leisure time. Optional excursions include a Kampala City Tour or Mabamba Shoebill Tour.",
-    listHeader: "Overnight Accommodation",
-    list: [
-      "Standard/Midrange: Gazelle Guest House – or similar",
-      "Luxury: Lake Victoria Serena Golf Resort – or similar"
-    ],
-    listHeader2: "Meal Plan",
-    list2: ["Bed and Breakfast"]
+    listHeader: "Meal Plan",
+    list: ["Bed and Breakfast"]
   },
   {
     question: "Day 2: Journey to Kibale with a Taste of Tradition",
     answer: "Begin the day with a scenic drive to Kibale National Park. Enjoy a traditional lunch at Tinka’s Homestead and explore the culture of the Bigodi Community with banana beer, coffee, cultural dances, and art.",
-    listHeader: "Overnight Accommodation",
-    list: [
-      "Standard: Chimpanzee Forest Lodge – or similar",
-      "Midrange: Turaco Treetops – or similar",
-      "Luxury: Chimpundu Lodge – or similar"
-    ],
-    listHeader2: "Meal Plan",
-    list2: ["Half Board (Dinner and Breakfast)"]
+    listHeader: "Meal Plan",
+    list: ["Half Board (Dinner and Breakfast)"]
   },
   {
     question: "Day 3: Into the Wild – Chimpanzee Habituation Adventure",
     answer: "Spend the day in Kibale National Park for a chimpanzee habituation experience, observing these intelligent primates closely. Encounter other wildlife, such as red-tailed and L’Hoest’s monkeys, rare butterflies, and over 300 bird species.",
-    listHeader: "Overnight Accommodation",
-    list: [
-      "Standard: Chimpanzee Forest Lodge – or similar",
-      "Midrange: Turaco Treetops – or similar",
-      "Luxury: Chimpundu Lodge – or similar"
-    ],
-    listHeader2: "Meal Plan",
-    list2: ["Full Board (Breakfast, Lunch, and Dinner)"]
+    listHeader: "Meal Plan",
+    list: ["Full Board (Breakfast, Lunch, and Dinner)"]
   },
   {
     question: "Day 4: Scenic Journey to Bwindi – Into the Heart of the Jungle",
     answer: "Travel to Bwindi Impenetrable National Park, a UNESCO World Heritage Site. Enjoy changing landscapes and a relaxing lunch en route before arriving at your lodge to soak in the forest atmosphere.",
-    listHeader: "Overnight Accommodation",
-    list: [
-      "Standard: Bweza Gorilla Lodge – or similar",
-      "Midrange: Ichumbi Gorilla Lodge – or similar",
-      "Luxury: Nkuringo Bwindi Gorilla Lodge – or similar"
-    ],
-    listHeader2: "Meal Plan",
-    list2: ["Full Board"]
+    listHeader: "Meal Plan",
+    list: ["Full Board"]
   },
   {
     question: "Day 5: Gorilla Habituation – A Day in the Wild",
     answer: "Join guides for a rare gorilla habituation experience, spending up to four hours observing a family of mountain gorillas in the dense forest and witnessing their unique behaviors.",
-    listHeader: "Overnight Accommodation",
-    list: [
-      "Standard: Bweza Gorilla Lodge – or similar",
-      "Midrange: Ichumbi Gorilla Lodge – or similar",
-      "Luxury: Nkuringo Bwindi Gorilla Lodge – or similar"
-    ],
-    listHeader2: "Meal Plan",
-    list2: ["Full Board"]
+    listHeader: "Meal Plan",
+    list: ["Full Board"]
   },
   {
     question: "Day 6: Golden Monkeys and Lake Mutanda’s Hidden Wonders",
     answer: "Start with a trek in Mgahinga Gorilla National Park to see the lively golden monkeys. Then, enjoy a peaceful boat cruise on Lake Mutanda, exploring islands, meeting local communities, and taking in views of the Virunga volcanoes.",
-    listHeader: "Overnight Accommodation",
-    list: ["Standard: Ichumbi Hotel Kisoro – or similar"],
-    listHeader2: "Meal Plan",
-    list2: ["Half Board (Dinner and Breakfast)"]
+    listHeader: "Meal Plan",
+    list: ["Half Board (Dinner and Breakfast)"]
   }
 ];
