4444ee238d3fd16164778a5ab9bf4b66423c6102
stanmelchizedek <stanexseven@gmail.com>
2025-05-29 16:25:44 +0300

    whatsapp button added

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -1,9 +1,10 @@
 import Navbar from "~/components/Navbar";
 import { Footer } from "~/components/Footer";
-import Head from "./Head";
+import { Toaster } from "./toaster";
+import { FaWhatsapp } from "react-icons/fa";
 
 import * as React from "react";
-import { Toaster } from "./toaster";
+import Link from "next/link";
 
 type Props = {
   children: React.ReactNode;
@@ -12,10 +13,26 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <>
-      <Navbar />
-      <Toaster />
-      <main>{children}</main>
-      <Footer />
+      <div>
+        <Navbar />
+        <Toaster />
+        <main className="relative">
+          {children}
+          <Link
+            className="p-4 bg-green-600 w-fit  overflow-hidden fixed bottom-6 right-8 z-20 rounded-full flex items-center justify-center hover:bg-green-700 duration-200 transition-colors"
+            href="https://wa.me/+255744400043"
+            target="_blank"
+          >
+            <div className="flex items-center gap-4 text-white">
+              <FaWhatsapp color="#fff" size={24} />
+              <p>
+                Chat with us on Whatsapp
+              </p>
+            </div>
+          </Link>
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
