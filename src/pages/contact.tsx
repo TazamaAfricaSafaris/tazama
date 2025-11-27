d8d67e120b6caafdd55db706b207abb86fe5e486
stanmelchizedek <stanexseven@gmail.com>
2025-11-27 13:22:26 +0300

    updated contact us page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -12,12 +12,13 @@ import { Toaster } from "~/components/ui/toaster";
 import { useToast } from "~/hooks/useToast";
 import { ToastAction } from "~/components/ui/Toast";
 import { Textarea } from "~/components/ui/textarea";
+import { IoArrowForward } from "react-icons/io5";
 
 const ContactPage = () => {
   const [email, setEmail] = React.useState<string>("");
   const [fullNames, setFullNames] = React.useState("");
   const [message, setMessage] = React.useState<string>("");
-  
+
   // Add validation states
   const [errors, setErrors] = React.useState({
     email: "",
@@ -90,10 +91,10 @@ const ContactPage = () => {
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // Prevent page reload
-    
+
     // Validate form before submission
     validateForm();
-    
+
     if (!formValid) {
       toast({
         variant: "destructive",
@@ -113,34 +114,57 @@ const ContactPage = () => {
 
   return (
     <>
-      <Toaster />
       <HeadSEO
         title="Contact Tazama Africa for your Dream Safari Experience"
         keywords={`${base_keywords}, Tanzania safari contact, book safari Tanzania, safari planning, Kilimanjaro trek booking, Tanzania travel inquiry, safari consultation, wildlife tour contact, African adventure planning, Tanzania tour operator, safari travel agent`}
         description="Contact Tazama Africa Safari to plan your dream Tanzania safari or Kilimanjaro trek. Our expert team is ready to help you create a personalized adventure through Africa's most spectacular landscapes and wildlife experiences."
       />
+      <Toaster />
       <PrimaryHeader image="contact.webp" title="Contact Us" />
-      <div className="mx-auto mt-36 max-sm:mt-28 px-8">
-        <div className="text-center">
-          <p className="mx-auto mb-10 mt-3 max-w-4xl px-4 text-5xl max-sm:text-3xl leading-[3.7rem] font-serif text-primary">
-            We'd love to hear from you. Simply fill in the form below or reach
-            out directly to us through one of our contacts listed below.
-          </p>
+      <div className="mx-auto mt-24 max-sm:mt-28 max-w-4xl px-4">
+        <div className="text-center mb-14">
+          <h3 className="text-5xl max-sm:text-3xl font-serif text-primary">
+            We'd Love to Here from You
+          </h3>
 
-          <p className="text-2xl max-sm:text-lg">
-            Email:&nbsp;
-            <a href="mailto:info@tazamaafricasafari.com">info@tazamaafricasafari.com</a>
-          </p>
-          <p className="text-2xl mt-2 max-sm:text-lg">
-            Phone:&nbsp;
-            <a href="tel:+255754922334">
-              (255)744400043
-            </a>
-          </p>
+          <p>You can get in touch with us for any more information, enquiries and questions</p>
+
+          <div className="grid grid-cols-2 gap-4 mt-8">
+            <div className="border border-light rounded-3xl text-left p-8 flex items-center justify-between group">
+              <div>
+                <p className="text-primary font-semibold text-lg mb-1">General Enquiries</p>
+                <p>
+                  <a className="hover:text-dark hover:underline hover:underline-offset-3" href="mailto:info@tazamaafricasafari.com">info@tazamaafricasafari.com</a><br />
+                  <a className="hover:text-dark hover:underline hover:underline-offset-3" href="tel:+255754922334">
+                    (255)754922334
+                  </a>
+                </p>
+              </div>
+              <div className=" border border-light rounded-full p-2">
+                <IoArrowForward className="-rotate-45 text-xl text-primary" />
+              </div>
+            </div>
+            <div className="border border-light rounded-3xl text-left p-8 flex items-center justify-between">
+              <div>
+                <p className="text-primary font-semibold">Reservations</p>
+                <p>
+                  <a className="hover:text-dark hover:underline hover:underline-offset-3" href="mailto:info@tazamaafricasafari.com">reservations@tazamaafricasafari.com</a><br />
+                  <a className="hover:text-dark hover:underline hover:underline-offset-3" href="tel:+255744400043">
+                    (255)744400043
+                  </a>
+                </p>
+              </div>
+              <div className=" border border-primary rounded-full p-2">
+                <IoArrowForward className="-rotate-45 text-xl text-primary" />
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div className="mx-auto my-14 max-w-2xl px-4 text-center">
-          <p className="text-lg">
+        <hr />
+
+        <div className="mx-auto my-12 max-w-2xl px-4 text-center">
+          <p>
             Please fill in the form below to get in touch with us and our team will get back to you as soon as possible.
           </p>
         </div>
